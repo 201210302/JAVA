e67c2b3c741691ba3087b43ca736b01aaf39940b
201210302 <90252213+201210302@users.noreply.github.com>
2021-11-18 19:10:23 +0900

    Add files via upload

---
+++ Printer.java
@@ -0,0 +1,62 @@
+abstract class Printer {
+    protected String model;
+    protected int printedCount;
+    protected int availableCount;
+
+    Printer(String model,int availableCount){
+        this.model= model;
+        this.printedCount=0;
+        this.availableCount=availableCount;
+    }
+
+    abstract public boolean print();
+}
+
+class InkjetPrinter extends Printer{
+
+    InkjetPrinter(String model, int availableCount){
+        super(model, availableCount);
+    }
+
+    @Override
+    public boolean print() {
+        if (availableCount == 0) {
+            System.out.println(model + ":" + (printedCount + 1) + "매째 인쇄 실패 - 잉크부족");
+            return false;
+        }
+        printedCount++;
+        availableCount--;
+
+        return true;
+    }
+
+}
+class LaserPrinter extends Printer{
+    LaserPrinter(String model, int availableCount){
+        super(model, availableCount);
+    }
+    @Override
+    public boolean print() {
+        if (availableCount==0){
+            System.out.println(model + ":" +(printedCount+1) +"매째 인쇄 실패 - 토너부족");
+            return false;
+        }
+        printedCount++;
+        availableCount--;
+
+        return true;
+
+    }
+}
+
+class Homework7 {
+    public static void main(String[] args) {
+        InkjetPrinter inkjet = new InkjetPrinter("Brother DCP-T720DW", 7500);
+        LaserPrinter laser = new LaserPrinter("Canon MFC643Cdw", 1500);
+        while (inkjet.print());
+        while (laser.print());
+    }
+}
+
+
+
